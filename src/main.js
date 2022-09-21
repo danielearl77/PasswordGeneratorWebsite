febccb4233ff62f83aaa94a570cfe0b94085564e
danielearl77 <danielearl@me.com>
2022-09-21 18:33:06 +0100

    UI updates CSS Updates

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import router from './router'
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
 
+import TabView from 'primevue/tabview';
+import TabPanel from 'primevue/tabpanel';
+
 import './assets/site.css'
 
 const app = createApp(App)
@@ -13,5 +16,8 @@ app.use(router)
 
 app.component('Navbar',Navbar)
 app.component('Footer',Footer)
+
+app.component('TabView', TabView)
+app.component('TabPanel',TabPanel)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
-<script setup>
-
-</script>
-
 <template>
   <main>
-    <h1>Home Page</h1>
+    <div id="passwords">
+      <TabView>
+        <TabPanel header="Password Generator">
+          Content I
+        </TabPanel>
+        <TabPanel header="Passphrase Generator">
+          Content II
+        </TabPanel>
+        <TabPanel header="PIN Generator">
+          Content III
+        </TabPanel>
+      </TabView>
+    </div>
   </main>
 </template>
+
+<script setup>
+  
+    
+</script>
+
+<style scoped>
+  #passwords {
+      width: 1080px;
+      margin: 100px auto;
+  }
+</style>
